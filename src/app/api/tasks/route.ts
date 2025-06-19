994af49441c7c2c0466c10cb8284d540d0f7ea7e
fabio_sirigu <fabio.sirigu@spindox.it>
2025-06-19 20:05:52 +0200

    feat: add POST and zod basic validation

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,7 +1,33 @@
 import { NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
+import { z } from "zod"
+
+const taskSchema = z.object({
+    title: z.string().min(1),
+    description: z.string().optional(),
+    status: z.enum(["PENDING", "IN_PROGRESS", "DONE"]),
+  })
 
 export async function GET() {
   const tasks = await prisma.task.findMany()
   return NextResponse.json(tasks)
 }
+
+export async function POST(request: Request) {
+    try {
+      const json = await request.json()
+      const data = taskSchema.parse(json)
+  
+      const newTask = await prisma.task.create({
+        data: {
+          title: data.title,
+          description: data.description,
+          status: data.status,
+        },
+      })
+  
+      return NextResponse.json(newTask, { status: 201 })
+    } catch (error) {
+      return NextResponse.json({ error: "Invalid data" }, { status: 400 })
+    }
+  }
